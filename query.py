13d6f7a7315f932b9c2822f4d1207ba9bb5d57a0
Taylor Watkins <twatki10@emich.edu>
2024-03-13 19:21:41 +0000

    feature: created a start menu
    created a python program that outputs a menu for the movie program
    
    *  so far, q for quit is the only menu option that functions
    
    issue #1

--- query.py
+++ query.py
@@ -1,3 +1,32 @@
 from movies import Movies
 
 movies = Movies('./movies.txt')
+
+print("Choose an option:")
+
+menu = {}
+menu['q']="enter q for exit" 
+menu['list']="enter list for listing all the movie names"
+menu['search']="enter search for searching movies"
+menu['cast']="enter cast for searching cast"
+while True: 
+    options=menu.keys()
+    for entry in options: 
+      print ('*', menu[entry])
+    print('\n')
+
+    x=input() 
+    if x =='q': 
+        break 
+    elif x == 'list': 
+        print('Come back later \n')
+    elif x == 'search':
+        print('Come back later \n')
+    elif x == 'cast':
+        print('Come back later \n')
+    else: 
+        print("Unknown option selected, try again \n")
+
+       
+
+
